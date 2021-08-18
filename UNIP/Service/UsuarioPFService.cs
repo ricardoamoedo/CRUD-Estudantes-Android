cc26018ab46d4fe5a66266b11838345c08a10d2d
ricardo amoedo <amoedo.ricardo@gmail.com>
2021-08-18 08:02:51 -0300

    adciionando crud ao repositório

---
+++ UNIP/Service/UsuarioPFService.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Policy;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Web.UI;
+using UNIP.Models;
+
+namespace UNIP.Service
+{
+    public class UsuarioPFService
+    {
+            public int Salvar(ServiceInterface.IUsuarioPF usuario)
+            {
+                var idRetorno = 0;
+                using (DB_UNIPPIMEntities4 save = new DB_UNIPPIMEntities4())
+                {
+                    try
+                    {
+                    Models.DT_Pessoa usuarioPessoa = new Models.DT_Pessoa();
+                    Models.DT_Endereco usuarioEndereco = new Models.DT_Endereco();
+                    Models.DT_Telefone usuarioTelefone = new Models.DT_Telefone();
+                    Models.DT_Telefone_Tipo usuarioTelefoneTipo = new Models.DT_Telefone_Tipo();
+
+                    usuarioPessoa.ID = usuario.ID;
+                    usuarioPessoa.NOME = usuario.Nome;
+                    usuarioPessoa.CPF = usuario.CPF;
+                    usuarioEndereco.ID = usuario.ID;
+                    usuarioPessoa.ENDERECO = usuarioEndereco.ID;
+
+
+                    var existeCPF = (from u in save.DT_Pessoa
+                                     where (u.CPF == usuario.CPF)
+                                     select u).FirstOrDefault();
+
+                    if (existeCPF == null )
+                        {
+                            save.DT_Pessoa.Add(usuarioPessoa);
+                            save.SaveChanges();
+                            idRetorno = usuarioPessoa.ID;
+                        }
+
+                        else
+                        {
+                            idRetorno = 0;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        return idRetorno;
+                        throw;
+                    }
+                }
+                return idRetorno;
+            }
+
+        }
+    }
---
+++ UNIP/ServiceInterface/IUsuarioPF.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography;
+using System.Dynamic;
+
+namespace UNIP.ServiceInterface
+{
+    public class IUsuarioPF
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Digite o Nome", AllowEmptyStrings = false)]
+        public string Nome { get; set; }
+
+        public int CPF { get; set; }
+
+        public string Endereco { get; set; }
+
+        public string logradouro { get; set; }
+
+        public string Numero { get; set; }
+
+        public string Cep { get; set; }
+
+        public string Bairro { get; set; }
+
+        public string Cidade { get; set; }
+
+        public string Estado { get; set; }
+
+        public string Telefone { get; set; }
+
+        public int ddd { get; set; }
+
+
+
+    }
+}
